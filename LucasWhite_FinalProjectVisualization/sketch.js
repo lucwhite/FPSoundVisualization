ae40b8a6dfc031fd4d9c85af862c2585e7ee7ef0
lucwhite <lw1944@nyu.edu>
2016-12-01 15:52:02 -0500

    Add files via upload

--- LucasWhite_FinalProjectVisualization/sketch.js
+++ LucasWhite_FinalProjectVisualization/sketch.js
@@ -2,17 +2,74 @@ var colorCount = 0;
 var speed;
 var value = 4;
 
+var osc, fft;
+
+var kick;
+var snare;
+
+var once;
+
+var speed;
+
+function preload() {
+  kick = loadSound('assets/808K01 Drums1DOTcom.wav');
+  snare = loadSound('assets/Snare12 Drums1DOTcom.wav');
+}
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(600, 600);
   noFill(0);
   stroke(60);
   strokeWeight(0.5);
 
+  soundFormats('wav');
+
+
+  osc = new p5.TriOsc(); // set frequency and type
+  osc.amp(.5);
+
+  fft = new p5.FFT();
+  osc.start();
+
 }
 
 function draw() {
+
   background(255);
-  duplicate();
+  duplicate(speed);
+
+  if (value === 0) {
+    var freq = 261.63;
+    
+
+  }
+  if (value == 1){ freq = 293.66;}
+  if (value == 2){ freq = 329.63;}
+  if (value == 3){ freq = 349.23;}
+  if (value == 4) {
+    freq = 392.00;
+    
+    
+  }
+  if (value == 5){ freq = 440.00;}
+  if (value == 6){ freq = 493.88;}
+  if (value == 7){ freq = 523.25;}
+  if (value == 8){ kick.play();}
+  if (value == 9){ snare.play();}
+
+  osc.freq(freq);
+  
+  var amp = map(mouseY, 0, height, 1, .01);
+  osc.amp(amp);
+  
+  
+  speed = map(mouseX, 0.1, width, 0, 2);
+  speed = constrain(speed, 0.01, 4);
+  snare.rate(speed);
+  snare.amp(amp/2);
+  kick.rate(speed);
+  kick.amp(amp/2);
+  
 
 }
 
@@ -25,12 +82,12 @@ function duplicate() {
     for (var y = 0; y < count; y++) {
       push();
       translate(x * size / 1.5, y * size / 1.5);
-      
+
       //stroke(255,0,0);
       //ellipse(0, 0, size, size);
       //strokeWeight(4);
       var v = int(255 * colorCount / 400.0)
-      if (value === 0) translate(-(frameCount % 15 + x % 2 + y % 2),(frameCount % 15 + x % 2 + y % 2));
+      if (value === 0) translate(-(frameCount % 15 + x % 2 + y % 2), (frameCount % 15 + x % 2 + y % 2));
       if (value == 1) rotate(-(frameCount * 0.04 + x / 2 + y / 2));
       if (value == 2) rotate(-(frameCount * 0.06 + x / 2 + y / 2));
       if (value == 3) rotate(-(frameCount * 0.08 + x / 2 + y / 2));
@@ -38,6 +95,8 @@ function duplicate() {
       if (value == 5) rotate(-(frameCount * 0.2 + x / 2 + y / 2));
       if (value == 6) rotate(-(frameCount * 0.36 + x / 2 + y / 2));
       if (value == 7) rotate(-(frameCount * 0.5 + x / 2 + y / 2));
+      if (value == 8) rotate(-(frameCount * speed + x / 2 + y / 2));
+      if (value == 9) rotate(-(frameCount * speed + x / 2 + y / 2));
       stroke(v, 25, 250);
       fill(v, 25, 250);
       ellipse((size / 3), (size / 3), 5, 5);
@@ -50,6 +109,14 @@ function duplicate() {
     }
   }
   colorCount = 0;
+}
+
+function mousePressed() {
+  if (mouseX > (width / 2)) {
+  kick.play();
+  } else {
+  snare.play();
+  }
 }
 
 function keyTyped() {
@@ -69,7 +136,17 @@ function keyTyped() {
     value = 6;
   } else if (key === 'k') {
     value = 7;
-  }
+  } else if (key === 'o') {
+    value = 8;
+  } else if (key === 'p') {
+    value = 9;
+  } 
   // uncomment to prevent any default behavior
   // return false;
+  
 }
+
+function keyReleased(){
+  value = 10;
+}
+
